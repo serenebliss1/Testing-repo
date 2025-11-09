263c8dcb24798145b812b1ff351f7494d265ad83
serenebliss0 <semajayi1234@gmail.com>
2025-11-09 19:39:50 +0100

    Weekly Sunday Commit

--- learning_csharp/csharp_practice/obj/Debug/net9.0/csharp_practice.AssemblyInfo.cs
+++ learning_csharp/csharp_practice/obj/Debug/net9.0/csharp_practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf5033d8b463e66292992f75c8436705167fb00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c75902c28b81d18c41bb9827615f6fdedd06ffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
